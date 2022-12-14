338ce46c37164721e25345769dcf812ab80bbff6
stuepfnick <stuepfnick@gmx.at>
2022-12-14 21:29:19 +0100

    day13 more cleanup

--- src/y2022/day13/Packet.java
+++ src/y2022/day13/Packet.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Packet implements Comparable<Packet>, Cloneable {
+public class Packet implements Comparable<Packet> {
 
     private final int number;
     private final List<Packet> packets = new ArrayList<>();
